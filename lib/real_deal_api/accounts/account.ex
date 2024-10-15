1b57a27f25131eec7547b286c9b73ca874f68144
Kevin Alejandro Morales Gomez <kevin.morales@ias.com.co>
2024-10-15 17:34:07 -0500

    se prueba la creacion de usuarios

---
+++ lib/real_deal_api/accounts/account.ex
@@ -0,0 +1,22 @@
+defmodule RealDealApi.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :hash_password, :string
+    has_one :user , RealDealApi.Users.User
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hash_password])
+    |> validate_required([:email, :hash_password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "debe tener arroba y no espacios")
+    |> unique_constraint(:email)
+  end
+end
